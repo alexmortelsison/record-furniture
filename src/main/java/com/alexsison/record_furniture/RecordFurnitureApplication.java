4644141cecbbf05b97e2c0699d4665307525a251
alexmortelsison <150509106+alexmortelsison@users.noreply.github.com>
2024-08-03 22:08:30 +0900

    first commit

---
+++ src/main/java/com/alexsison/record_furniture/RecordFurnitureApplication.java
@@ -0,0 +1,13 @@
+package com.alexsison.record_furniture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecordFurnitureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecordFurnitureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alexsison/record_furniture/RecordFurnitureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexsison.record_furniture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecordFurnitureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
